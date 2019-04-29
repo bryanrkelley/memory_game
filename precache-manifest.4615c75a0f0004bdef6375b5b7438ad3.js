2d37d168d78ef54e62e8c68a97f51227881445ce
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-04-28 23:09:27 -0700

    Updates

---
+++ precache-manifest.4615c75a0f0004bdef6375b5b7438ad3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee702a02d3c931986fcfc78ec22db8db",
+    "url": "/memory_game/index.html"
+  },
+  {
+    "revision": "22f50b5daa791bd9faa2",
+    "url": "/memory_game/static/css/main.c96e0c2c.chunk.css"
+  },
+  {
+    "revision": "ae3ca211f25ee207a154",
+    "url": "/memory_game/static/js/2.ba409389.chunk.js"
+  },
+  {
+    "revision": "22f50b5daa791bd9faa2",
+    "url": "/memory_game/static/js/main.23a30ba9.chunk.js"
+  },
+  {
+    "revision": "a6def2a3b08ffe9cb26d",
+    "url": "/memory_game/static/js/runtime~main.aa57f519.js"
+  }
+]);
